bfb371c2d7a8eadfdbd939c533960f975b4608af
Torgeir Lebesbye <torgeir@lebesbye.com>
2016-08-29 11:53:45 +0200

    Removed unnecessary lower()

--- bot/messenger.py
+++ bot/messenger.py
@@ -24,7 +24,6 @@ def emojiFilter(input):
 		start = lastpos + 2
 		end = ret.rfind("_:")
 		content = ret[start:end]
-		content = content.lower()
 		content = content.replace("/", "")
 		ret = ret[:start] + content + ret[end:]
 	return ret
